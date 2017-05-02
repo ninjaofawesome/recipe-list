7a43edff289c63e2503f8c1bf1d78f81bb28ae36
Hannah Nordgren <hey@hereshannahs.info>
2017-05-01 23:55:02 -0400

    [FEATURE] hidden and visible toggle menu in nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
+
+
 class Nav extends Component {
+
+  constructor(props) {
+    super(props);
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.state = { "active" : false };
+  }
+
+  componentDidMount() {
+    this.toggleMenu();
+  }
+
+  toggleMenu() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+
+    const hiddenList = document.querySelector('.recipe-list__navbar-list');
+
+    if (currentState === true) {
+      hiddenList.classList.toggle('recipe-list__navbar-list--visible');
+    }
+  }
+
   render() {
     return (
       <nav className="recipe-list__navbar">
         <h1 className="recipe-list__navbar-title">Recipe List</h1>
-        <button className="recipe-list__navbar-menu-button">menu</button>
+        <button className="recipe-list__navbar-menu-button" onClick={this.toggleMenu} >menu</button>
         <ul className="recipe-list__navbar-list">
           <li className="recipe-list__navbar-list-item">Favorites</li>
           <li className="recipe-list__navbar-list-item">Most Recent</li>
