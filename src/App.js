5b4bb4d41b7cf35b199a29f7b6dfc1b0e4873337
Hannah Nordgren <hey@hereshannahs.info>
2017-05-04 15:31:02 -0400

    [FEATURE] initial adding of props to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
 import Nav from '../src/components/nav/nav';
 import List from '../src/components/list/list';
+import $ from 'jquery';
+import moment from 'moment';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {recipeCard:[]};
+  }
+
+  componentDidMount() {
+    let url = "https://api.nytimes.com/svc/topstories/v2/food.json";
+    url += '?' + $.param({
+      'api-key': "4c0279d4d0ec494eb035ac5281200c59"
+    });
+    return $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      this.setState({ recipeCard: result.results});
+    }.bind(this)).fail(function(err) {
+      throw err;
+    });
+  }
+
   render() {
+
+    const cardData = this.state.recipeCard.map((item, index) => {
+      const foodSection = item.section === 'Food';
+      const published = moment(item.published_date).format("dddd, MMMM Do YYYY");
+      const image = item.multimedia.slice(2,3);
+
+      if (image.length === 0) {
+        image.push({
+          "url" : "https://res.cloudinary.com/ninjaofawesome/image/upload/c_scale,h_127,w_190/v1493131754/hannah/projects/listicle/food-rainbow.jpg",
+          "alt" : "Placeholder Food Image",
+          "width": 190,
+          "height": 127
+        })
+      }
+
+      const cardObj = {
+        key: index,
+        section: foodSection,
+        title: item.title,
+        byline: item.byline,
+        published_date: published,
+        url: item.url,
+        multimedia: image
+      }
+      return cardObj;
+    });
+
+
     return (
       <div className="recipe-list">
         <Nav />
-        <List />
+        <List cardInfo={cardData} />
       </div>
     );
   }
